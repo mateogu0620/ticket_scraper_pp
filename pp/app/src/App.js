adb4baa3fcfcec7d4bbfe2de478a88f22a8ca769
NiravGolyalla <ng2411@nyu.edu>
2023-04-27 10:29:32 -0400

    Navbar update

--- pp/app/src/App.js
+++ pp/app/src/App.js
@@ -48,15 +48,16 @@ function App() {
           <Navbar.Brand href="#home">Brand link</Navbar.Brand>
         </Container>
       </Navbar> */}
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
           <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home">Ticket Scraper</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                
+              </Nav>
+              <Nav>
+              <NavDropdown title="Profile" id="collasible-nav-dropdown" >
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.2">
                     Another action
@@ -64,15 +65,9 @@ function App() {
                   <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="#action/3.4">
-                    Separated link
+                    Log out
                   </NavDropdown.Item>
                 </NavDropdown>
-              </Nav>
-              <Nav>
-                <Nav.Link href="#deets">More deets</Nav.Link>
-                <Nav.Link eventKey={2} href="#memes">
-                  Dank memes
-                </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
