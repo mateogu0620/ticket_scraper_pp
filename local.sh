553136973fa59e4d7f80654b95d5eb74914bb79f
mateogu0620 <mateogarcia0620@gmail.com>
2023-05-01 15:30:52 +0200

    fixed local and prod shell scripts, cleaned up makefile

--- local.sh
+++ local.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # run our front-end against local server
+cd pp/app
 export REACT_APP_BACKEND_BASE_URL=http://127.0.0.1:8000
+echo "Running front-end on the local backend, i.e. $REACT_APP_BACKEND_BASE_URL"
 npm start
--- prod.sh
+++ prod.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # run our front-end against prod server
+cd pp/app
 export REACT_APP_BACKEND_BASE_URL=http://niravgolyalla.pythonanywhere.com
+echo "Running front-end on the prod backend, i.e. $REACT_APP_BACKEND_BASE_URL"
 npm start
